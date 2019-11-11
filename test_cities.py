64e2a91bd92e702ec01df77968e96550581f1f3b
Mark Sherrington <git@marksherrington.uk>
2019-11-11 18:36:07 +0000

    meaningful logic for test_swap_cities

--- test_cities.py
+++ test_cities.py
@@ -13,8 +13,18 @@ def test_compute_total_distance():
 
     '''add your further tests'''
 
+
 def test_swap_cities():
-    '''add your tests'''
+    index1 = 0
+    index2 = 1
+    road_map = [
+        ("Kentucky", "Frankfort", 38.197274, -84.86311),
+        ("Delaware", "Dover", 39.161921, -75.526755),
+        ("Minnesota", "Saint Paul", 44.95, -93.094)
+    ]
+    swapped = road_map[:]
+    swapped[index1], swapped[index2] = swapped[index2], swapped[index1]
+    assert swap_cities(road_map, index1, index2) == swapped
 
 def test_shift_cities():
     '''add your tests'''
