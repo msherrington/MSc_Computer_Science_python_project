b548a22611fb68ac0003f272c701aa297539116f
Mark Sherrington <git@marksherrington.uk>
2019-11-11 18:34:53 +0000

    meaningful logic for test_compute_total_distance()

--- test_cities.py
+++ test_cities.py
@@ -3,12 +3,13 @@ from cities import *
 
 
 def test_compute_total_distance():
-    road_map1 = [("Kentucky", "Frankfort", 38.197274, -84.86311),\
-                ("Delaware", "Dover", 39.161921, -75.526755),\
-                ("Minnesota", "Saint Paul", 44.95, -93.094)]
+    road_map1 = [
+        ("Kentucky", "Frankfort", 38.197274, -84.86311),
+        ("Delaware", "Dover", 39.161921, -75.526755),
+        ("Minnesota", "Saint Paul", 44.95, -93.094)
+    ]
     
-    assert compute_total_distance(road_map1)==\
-           pytest.approx(9.386+18.496+10.646, 0.01)
+    assert compute_total_distance(road_map1) == pytest.approx(9.386+18.496+10.646, 0.01)
 
     '''add your further tests'''
 
