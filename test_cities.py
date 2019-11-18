3738f558a26714d6941ba7a696dcda4f594277d9
Mark Sherrington <git@marksherrington.uk>
2019-11-18 21:04:40 +0000

    adding test_get_two_random_indices

--- test_cities.py
+++ test_cities.py
@@ -36,6 +36,11 @@ def test_shift_cities(road_map):
     assert shift_cities(road_map) == ([road_map[-1]] + road_map[:-1])
 
 
+def test_get_two_random_indices(road_map_length):
+    a, b = get_two_random_indices(road_map_length-1)
+    assert 0 < a < road_map_length and 0 < b < road_map_length
+
+
 def test_read_cities(file_name, road_map):
     """
     Included this test (even though it's not required) to
