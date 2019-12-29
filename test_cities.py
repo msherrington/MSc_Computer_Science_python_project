b612ffa7410831ea1e5fdd4c1bb1e86559ee4cfd
Mark Sherrington <git@marksherrington.uk>
2019-12-29 17:01:12 +0000

    re-order functions in test file

--- test_cities.py
+++ test_cities.py
@@ -1,10 +1,5 @@
 import pytest
-from cities import (
-    compute_total_distance,
-    read_cities,
-    shift_cities,
-    swap_cities
-)
+from cities import compute_total_distance, shift_cities, swap_cities
 from services import (
     can_be_floated,
     euclidean_distance,
@@ -15,9 +10,11 @@ from services import (
 )
 
 
+# ___ FIXTURES _____
 
 @pytest.fixture
 def road_map():
+    """ Reusable test data """
     return [
         ('California', 'Sacramento', 38.555605, -121.468926),
         ('Illinois', 'Springfield', 39.78325, -89.650373),
@@ -34,6 +31,8 @@ def road_map_errors():
     }
 
 
+# ___ UTILITY FUNCTIONS _____
+
 def assert_for_raised_error(error_type, func, params, message):
     """
     Reusable function to assert raised errors
@@ -49,17 +48,7 @@ def assert_for_raised_error(error_type, func, params, message):
     assert str(e.value) == message
 
 
-def test_round_coordinates(road_map):
-    coord = road_map[0][2]
-    rounded = round(coord, 2)
-    assert round_coordinates(coord) == rounded
-    assert round_coordinates(str(coord)) == rounded
-    assert round_coordinates(66) == 66.0
-    assert isinstance(round_coordinates(66), float)
-    assert round_coordinates(12.3456789) == pytest.approx(12.34, 0.01)
-    assert_for_raised_error(ValueError, round_coordinates, ['string'], 'Cannot float coordinate')
-    assert_for_raised_error(TypeError, round_coordinates, [[]], 'Coordinate must be string or number')
-
+# ___ TESTS FOR REQUIRED FUNCTIONS _____
 
 def test_compute_total_distance(road_map, road_map_errors):
     assert compute_total_distance(road_map) == pytest.approx(31.842+30.110+1.819, 0.01)
@@ -75,19 +64,6 @@ def test_compute_total_distance(road_map, road_map_errors):
                             'Coordinates must be of type float')
     assert_for_raised_error(TypeError, compute_total_distance, [[(38.555605, -121.468926, 38.555605, -121.468926)]],
                             'City and State must be strings')
-
-
-def test_euclidean_distance(road_map):
-    coord_one, coord_two = road_map[0][-2:], road_map[1][-2:]
-    assert isinstance(euclidean_distance(coord_one, coord_two), float)
-    assert euclidean_distance(coord_one, coord_two) == pytest.approx(31.842, 0.01)
-    assert euclidean_distance(coord_one, coord_one) == 0.0
-    assert_for_raised_error(TypeError, euclidean_distance, [coord_one, 'string'],
-                            'Coordinates must be packed in tuples or lists')
-    assert_for_raised_error(ValueError, euclidean_distance, [coord_one, (38.6, 45.2, 38.4)],
-                            'Exactly two elements required in location tuple')
-    assert_for_raised_error(TypeError, euclidean_distance, [coord_one, (38.6, '45.2',)],
-                            'Coordinates must be a number')
 
 
 def test_swap_cities(road_map, road_map_errors):
@@ -124,6 +100,42 @@ def test_shift_cities(road_map, road_map_errors):
     assert_for_raised_error(IndexError, shift_cities, [[]], road_map_errors['empty'])
 
 
+# ___ TESTS FOR ADDITIONAL FUNCTIONS _____
+
+def test_can_be_floated():
+    assert can_be_floated(5)
+    assert can_be_floated('5')
+    assert can_be_floated(5.0)
+    assert can_be_floated('5.0')
+    assert not can_be_floated('text string')
+    assert not can_be_floated([5])
+
+
+def test_round_coordinates(road_map):
+    coord = road_map[0][2]
+    rounded = round(coord, 2)
+    assert round_coordinates(coord) == rounded
+    assert round_coordinates(str(coord)) == rounded
+    assert round_coordinates(66) == 66.0
+    assert isinstance(round_coordinates(66), float)
+    assert round_coordinates(12.3456789) == pytest.approx(12.34, 0.01)
+    assert_for_raised_error(ValueError, round_coordinates, ['string'], 'Cannot float coordinate')
+    assert_for_raised_error(TypeError, round_coordinates, [[]], 'Coordinate must be string or number')
+
+
+def test_euclidean_distance(road_map):
+    coord_one, coord_two = road_map[0][-2:], road_map[1][-2:]
+    assert isinstance(euclidean_distance(coord_one, coord_two), float)
+    assert euclidean_distance(coord_one, coord_two) == pytest.approx(31.842, 0.01)
+    assert euclidean_distance(coord_one, coord_one) == 0.0
+    assert_for_raised_error(TypeError, euclidean_distance, [coord_one, 'string'],
+                            'Coordinates must be packed in tuples or lists')
+    assert_for_raised_error(ValueError, euclidean_distance, [coord_one, (38.6, 45.2, 38.4)],
+                            'Exactly two elements required in location tuple')
+    assert_for_raised_error(TypeError, euclidean_distance, [coord_one, (38.6, '45.2',)],
+                            'Coordinates must be a number')
+
+
 def test_random_index(road_map):
     maximum = len(road_map)-1
     random = random_index(maximum)
@@ -133,17 +145,6 @@ def test_random_index(road_map):
     assert_for_raised_error(TypeError, random_index, [23.0], 'Maximum must be an integer')
     assert_for_raised_error(TypeError, random_index, ['string'], 'Maximum must be an integer')
     assert_for_raised_error(ValueError, random_index, [-2], 'Maximum must be a positive integer')
-
-
-
-
-def test_can_be_floated():
-    assert can_be_floated(5)
-    assert can_be_floated('5')
-    assert can_be_floated(5.0)
-    assert can_be_floated('5.0')
-    assert not can_be_floated('text string')
-    assert not can_be_floated([5])
 
 
 def test_validate_road_map(road_map, road_map_errors):
